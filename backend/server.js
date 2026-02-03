d362745893d62a56cbd0d38b4c3f75866861a507
mitiacelo <157123349+mitiacelo@users.noreply.github.com>
2026-02-03 20:31:14 +0300

    Premier commit - initialisation du projet

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connecté ✅"))
+  .catch(err => console.error("Erreur de connexion MongoDB :", err));
+
+ // Routes
+app.use('/articles', require('./routes/articleRoutes'));
+app.listen(PORT, () => console.log(`Serveur démarré sur le port
+${PORT}`));
